6efff48f761c43b71fef2575ed1db91fe2919dc9
Witty Chang <wcc1213@gmail.com>
2017-09-18 08:41:48 -0700

    Integrate shared preferences.

--- app/src/main/java/com/epicodus/myrestaurants/ui/MainActivity.java
+++ app/src/main/java/com/epicodus/myrestaurants/ui/MainActivity.java
@@ -1,6 +1,8 @@
 package com.epicodus.myrestaurants.ui;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -9,16 +11,21 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.graphics.Typeface;
 
+import com.epicodus.myrestaurants.Constants;
 import com.epicodus.myrestaurants.R;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private SharedPreferences mSharedPreferences;
+    private SharedPreferences.Editor mEditor;
+
 //    public static final String TAG = MainActivity.class.getSimpleName();
-    @Bind(R.id.findRestaurantsButton) Button mFindRestaurantsButton;
-    @Bind(R.id.locationEditText) EditText mLocationEditText;
     @Bind(R.id.textView) TextView mAppNameTextView;
+    @Bind(R.id.locationEditText) EditText mLocationEditText;
+    @Bind(R.id.findRestaurantsButton) Button mFindRestaurantsButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,21 +33,39 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
 
+//        mAppNameTextView = (TextView) findViewById(R.id.textView);
+
         Typeface champagneFont = Typeface.createFromAsset(getAssets(), "fonts/cac_champagne.ttf");
         mAppNameTextView.setTypeface(champagneFont);
 
-        mLocationEditText = (EditText) findViewById(R.id.locationEditText);
-//        mFindRestaurantsButton = (Button) findViewById(R.id.findRestaurantsButton);
-        mFindRestaurantsButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String location = mLocationEditText.getText().toString();
-                Intent intent = new Intent(MainActivity.this, RestaurantListActivity.class);
-                intent.putExtra("location", location);
-                startActivity(intent);
-            }
-        });
-//        mAppNameTextView = (TextView) findViewById(R.id.textView);
+        mSharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        mEditor = mSharedPreferences.edit();
 
+//        mLocationEditText = (EditText) findViewById(R.id.locationEditText);
+
+        mFindRestaurantsButton.setOnClickListener(this);
+    }
+
+//        mFindRestaurantsButton = (Button) findViewById(R.id.findRestaurantsButton);
+
+    @Override
+    public void onClick(View v) {
+        if (v == mFindRestaurantsButton) {
+
+//        mFindRestaurantsButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+            String location = mLocationEditText.getText().toString();
+            if(!(location).equals("")) {
+                addToSharedPreferences(location);
+            }
+            Intent intent = new Intent(MainActivity.this, RestaurantListActivity.class);
+            intent.putExtra("location", location);
+            startActivity(intent);
+        }
+    }
+
+    private void addToSharedPreferences(String location) {
+        mEditor.putString(Constants.PREFERENCES_LOCATION_KEY, location).apply();
     }
 }
--- app/src/main/java/com/epicodus/myrestaurants/ui/RestaurantListActivity.java
+++ app/src/main/java/com/epicodus/myrestaurants/ui/RestaurantListActivity.java
@@ -1,11 +1,15 @@
 package com.epicodus.myrestaurants.ui;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 
+import com.epicodus.myrestaurants.Constants;
 import com.epicodus.myrestaurants.R;
 import com.epicodus.myrestaurants.adapters.RestaurantListAdapter;
 import com.epicodus.myrestaurants.models.Restaurant;
@@ -21,7 +25,10 @@ import java.io.IOException;
 import java.util.ArrayList;
 
 public class RestaurantListActivity extends AppCompatActivity {
-    public static final String TAG = RestaurantListActivity.class.getSimpleName();
+    private SharedPreferences mSharedPreferences;
+    private String mRecentAddress;
+
+//    public static final String TAG = RestaurantListActivity.class.getSimpleName();
 
     @Bind(R.id.recyclerView) RecyclerView mRecyclerView;
     private RestaurantListAdapter mAdapter;
@@ -43,8 +50,17 @@ public class RestaurantListActivity extends AppCompatActivity {
 
 //        mLocationTextView.setText("Here are all the restaurants near: " + location);
 
-        getRestaurants(location);
+//        getRestaurants(location);
+
+        mSharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        mRecentAddress = mSharedPreferences.getString(Constants.PREFERENCES_LOCATION_KEY, null);
+
+        if (mRecentAddress != null) {
+            getRestaurants(mRecentAddress);
         }
+
+        Log.d("Shared Pref Location", mRecentAddress);
+    }
 
 //        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, restaurants);
 //        mListView.setAdapter(adapter);
@@ -113,7 +129,6 @@ public class RestaurantListActivity extends AppCompatActivity {
 //                            Log.d(TAG, "Categories: " + restaurant.getCategories().toString());
 //                        }
                     }
-
                 });
 
 //                try {
@@ -128,5 +143,4 @@ public class RestaurantListActivity extends AppCompatActivity {
             }
         });
     }
-
 }
